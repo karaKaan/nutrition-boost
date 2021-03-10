c30cf3bf1dd8929e44f8708330d87614b3322160
karaKaan <kara.kaan1995@gmail.com>
2021-03-10 16:22:38 +0100

    created a react-app and first looking of the navbar

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+
+import logo from './images/logo.svg'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar 
+            isLogo={true}
+            logoUrl={logo}
+            navList1="NavList1" 
+            navList2="NavList2" 
+            navList3="NavList3" 
+            navList4="NavList4"
+            isLogin={true}
+            navListLogin="Register"
+            />
+        <Router />
+      </Router>
+    </>
+    );
+}
+
+export default App;
